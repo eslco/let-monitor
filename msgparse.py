9d05ae444ba7aa162403012db72d12ddb3ec171c
vpslog <admin@vpslog.org>
2025-11-26 07:24:51 +0000

    fix parse error

--- msgparse.py
+++ msgparse.py
@@ -5,8 +5,12 @@ def thread_message(thread, ai_description):
         f"标题：{thread['title']}\n"
         f"作者：{thread['creator']}\n"
         f"时间：{thread['pub_date'].strftime('%Y/%m/%d %H:%M')}\n\n"
-        # f"{thread['description'][:200].strip()}...\n\n"
-        + (f"{ai_description[:200].strip()}{"..." if len(ai_description)>200 else ''}\n\n" if ai_description else "")
+        + (
+            f"{ai_description[:200].strip()}"
+            + ("..." if len(ai_description) > 200 else "")
+            + "\n\n"
+            if ai_description else ""
+        )
         + f"{thread['link']}"
     )
     return msg
@@ -17,7 +21,12 @@ def comment_message(thread, comment, ai_description):
         f"作者：{comment['author']}\n"
         f"时间：{comment['created_at'].strftime('%Y/%m/%d %H:%M')}\n\n"
         f"{comment['message'][:200].strip()}...\n\n"
-        + (f"{ai_description[:200].strip()}...\n\n" if ai_description else "")
+        + (
+            f"{ai_description[:200].strip()}"
+            + ("..." if len(ai_description) > 200 else "")
+            + "\n\n"
+            if ai_description else ""
+        )
         + f"{comment['url']}"
     )
     return msg
